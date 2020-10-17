f6adf6ac510de77016ce1f6847a99c6a3b2ea91e
JeremyDaug <jeremy.daug@gmail.com>
2020-10-17 17:21:32 -0500

    Added Randomizer Class and Interface

---
+++ EconomicCalculator/Randomizer/IRandomizer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EconomicCalculator.Randomizer
+{
+    public interface IRandomizer
+    {
+        int Next();
+
+        int Next(int maxValue);
+
+        int Next(int minValue, int MaxValue);
+
+        double NextDouble();
+
+        void NextByte(byte[] buffer);
+
+        double DoubleBetweenValues(double min, double max);
+
+        double Normal(double mean, double stdDev);
+
+        double Normal();
+
+        double Exponential(double rate);
+    }
+}
