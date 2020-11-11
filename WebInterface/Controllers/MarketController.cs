cc601d1dea3407143dc36efd7370455f0690600d
JeremyDaug <jeremy.daug@gmail.com>
2020-11-11 14:05:00 -0600

    Started work on MVC interface. Will merge when tutorial is done.

---
+++ WebInterface/Controllers/MarketController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebInterface.Controllers
+{
+    public class MarketController : Controller
+    {
+        // GET: Market
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // GET: Market/Welcome
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+    }
+}
