9270cabcaa3cabc2742d9a47602a33bae3272d5c
JeremyDaugherty <Jeremy.Daug@gmail.com>
2022-12-23 19:17:52 -0600

    Small update to pops

--- political_economy_core/src/objects/pop.rs
+++ political_economy_core/src/objects/pop.rs
@@ -4,6 +4,8 @@
 //! populations.
 use std::collections::HashMap;
 
+use super::desires::Desires;
+
 
 /// Pops are the data storage for a population group.
 /// 
@@ -12,19 +14,27 @@ use std::collections::HashMap;
 /// what their job in that firm is.
 /// 
 /// Internally they are broken appart by the various of the
-/// pop. It breaks them into a table of 
+/// pop. It breaks them into a table to record details of how many are in each species/culture combo.
 #[derive(Debug)]
 pub struct Pop {
+    /// Pop's unique id for navigation purposes.
     pub id: usize,
-    pub name: String,
+    /// Pop's total population (should be equal to the sum of it's breakdown table.)
     pub count: usize,
+    /// The job of the pop.
     pub job: usize,
+    /// Where the pop works.
     pub firm: usize,
+    /// Which market they are in
     pub market: usize,
+    /// The skill the pop uses.
     pub skill: usize,
+    /// The lower bound of their skill level.
     pub lower_skill_level: f64,
+    /// the upper bound of their skill level spread.
     pub higher_skill_level: f64,
-    pub property: HashMap<usize, f64>,
+    /// The total desires and property of the pop.
+    pub desires: Desires,
     pub species: HashMap<usize, usize>,
     // civilization
     pub culture: HashMap<usize, usize>
@@ -36,4 +46,8 @@ pub struct Pop {
 }
 
 impl Pop {
+}
+
+pub struct PopBreakdownTable {
+
 }
